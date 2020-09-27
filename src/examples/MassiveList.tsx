f58d8bcdd94cf62086e6c9b62b901693d0824335
drrefactor <drrefactor@gmail.com>
2020-09-27 23:14:27 +0200

    Add 'MassiveList' example usage for RenderPool

---
+++ src/examples/MassiveList.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import { RenderPool, RenderPoolChild } from "../render-pool/RenderPool"
+import { range } from "../utils"
+
+export const MassiveList: React.FC = () => {
+  return (
+    <RenderPool>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          background: 'rgba(0, 0, 0, 0.8)',
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          alignContent: 'flex-start'
+        }}
+      >
+        {range(15000).map(i => (
+          <RenderPoolChild key={i}>
+            <div
+              style={{
+                borderWidth: 1,
+                borderColor: 'cyan',
+                borderRadius: 40,
+                borderStyle: 'solid',
+                width: '25px',
+                height: '25px',
+                margin: 2,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              <div
+                style={{fontSize: 8, color: 'white'}}
+              >{i}</div>
+            </div>
+          </RenderPoolChild>
+        ))}
+      </div>
+    </RenderPool>
+  )
+}
