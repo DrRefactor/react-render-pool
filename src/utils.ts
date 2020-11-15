ad5e75c286f098e07ea96a11adfb63b86ac2a571
drrefactor <drrefactor@gmail.com>
2020-11-15 02:47:18 +0100

    Implement delay util

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,7 @@ export function noop() {}
 export function range(length: number) {
   return (new Array(length)).fill(null).map((_, i) => i);
 }
+
+export function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
